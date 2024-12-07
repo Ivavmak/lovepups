e54c29f8329eb672d752deb2efca0e3f6f2c6201
Ivan_makuryn <77403884+Ivavmak@users.noreply.github.com>
2024-12-08 02:36:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const heartContainer = document.getElementById('heart-container');
+const phraseCounter = document.getElementById('phraseCount');
+
+const phrases = [
+    'Люблю тебя!',
+    'Маленькая моя!',
+    'Самая лучшая!',
+    'Я всегда рядом!',
+    'Бусинка!',
+    'Пупа моя!',
+    'Принцесска!',
+    'Самая красивая!',
+    'Свети ярче!',
+    'Я тебя обожаю!',
+	'Мои<br>156см счастья!',
+	'Против зелёных глаз<br>иммунитета нет!',
+	'Самая красивая<br>вишенка!',
+	'Безумно<br>люблю тебя!',
+	'Карим глазам<br>суждено быть<br>с зелёными!',
+	'Заобнимаю!',
+	'Люблю!'
+];
+
+function getRandomPhrase() {
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    return phrases[randomIndex];
+}
+
+function createHeart(count) {
+    for (let i = 0; i < count; i++) {
+        const heartDiv = document.createElement('div');
+        heartDiv.className = 'heart';
+        
+        heartDiv.innerHTML = '❤️';
+        
+        const phraseLink = document.createElement('a');
+        phraseLink.innerHTML = getRandomPhrase();
+        
+        heartDiv.appendChild(phraseLink);
+        
+        heartContainer.appendChild(heartDiv);
+    }
+}
+
+function handleScroll() {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition >= documentHeight - 600) {
+        createHeart(50);
+        console.log(heartsCount);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+phraseCounter.textContent = 'Всего: ' + phrases.length + ' надписей';
+createHeart(100);
